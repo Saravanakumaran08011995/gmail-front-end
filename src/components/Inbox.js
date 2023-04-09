253b90ef0e5b863cbce6a2791fc01ce6e5362d22
Saravanakumaran08011995 <saravanakumaran8195@gmail.com>
2023-04-10 00:14:28 +0530

    changes

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -9,7 +9,7 @@ const Inbox = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get('https://saravana-kumaran-gmail-backend-j5s45brtm.vercel.app/api/emails');
+      const response = await axios.get('http://saravana-kumaran-gmail-backend-j5s45brtm.vercel.app/api/emails');
       setEmails(response.data);
     };
 
